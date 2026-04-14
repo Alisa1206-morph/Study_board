feb3aaca672866247dd12701cdb17c9a0e0ab653
Alisa1206-morph <alisa6efremenko2007@gmail.com>
2026-04-14 08:55:24 +0300

    migrations for ProjectType changes and FixPendingChanges

---
+++ Study_board.Data/Migrations/20260329112820_ProjectTypeAdd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Study_board.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectTypeAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Projects",
+                newName: "ProjectType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProjectType",
+                table: "Projects",
+                newName: "Type");
+        }
+    }
+}
